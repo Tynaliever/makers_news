739f5a21a697c016f51f6a08719907140e8b5d5b
nomadec <umidjan.kushayev@gmail.com>
2021-11-16 15:47:09 +0600

    checkpoint

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,33 @@
-// ! CREATE
-// ! Create news button from Navbar opens Modal Window
-// ! On modal window when inputs are filled, collect and POST
-// ! As user i want to be able select between Yellow page or Ordinary News
+document.addEventListener('DOMContentLoaded', () => {
+  // ! CREATE
 
-// ! READ
-// ! Details Block should render TOP news
-// ! Ordinary news to render REST news
-// ! Yellow pages to render YELLOW news
+  // ! Create news button from Navbar opens Modal Window
+  // ! On modal window when inputs are filled, collect and POST
+  const postBtn = document.getElementById('create_news');
+  document.getElementById('news_title');
+  document.getElementById('news_text');
+  document.getElementById('news_is_yellow');
 
-// ! UPDATE
-// ! When mouseover the news display Edit button
-// ! when clicked open Modal Window and pre-fill inputs
 
-// ! DELETE
-// ! When mouseover the news display DELETE button
-// ! When clicked just delete it
 
-// ! NavItem onHover show Menu
-// ! Search at last if got time
+
+
+
+  // ! As user i want to be able select between Yellow page or Ordinary News
+
+  // ! READ
+  // ! Details Block should render TOP news
+  // ! Ordinary news to render REST news
+  // ! Yellow pages to render YELLOW news
+
+  // ! UPDATE
+  // ! When mouseover the news display Edit button
+  // ! when clicked open Modal Window and pre-fill inputs
+
+  // ! DELETE
+  // ! When mouseover the news display DELETE button
+  // ! When clicked just delete it
+
+  // ! NavItem onHover show Menu
+  // ! Search at last if got time
+});
