fe0019fe9889e6a15a12c8f2d401350ca49008e4
nomadec <umidjan.kushayev@gmail.com>
2021-11-16 15:03:18 +0600

    structure html

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+// ! CREATE
+// ! Create news button from Navbar opens Modal Window
+// ! On modal window when inputs are filled, collect and POST
+// ! As user i want to be able select between Yellow page or Ordinary News
+
+// ! READ
+// ! Details Block should render TOP news
+// ! Ordinary news to render REST news
+// ! Yellow pages to render YELLOW news
+
+// ! UPDATE
+// ! When mouseover the news display Edit button
+// ! when clicked open Modal Window and pre-fill inputs
+
+// ! DELETE
+// ! When mouseover the news display DELETE button
+// ! When clicked just delete it
+
+// ! NavItem onHover show Menu
+// ! Search at last if got time
